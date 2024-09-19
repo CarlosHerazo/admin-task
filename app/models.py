7472f7235d020700e15ca2b2126d51cd2ba31720
CarlosHerazo <Carlosherazo2004@gmail.com>
2024-09-19 11:23:44 -0500

    migraciones de tablas

--- app/models.py
+++ app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    state = models.ForeignKey('State', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'tareas'
+
+
+class State(models.Model):
+    name = models.CharField(
+        max_length=50,
+        choices=[
+            ('scheduled', 'Scheduled'),
+            ('to finish', 'To finish'),
+            ('compliment', 'Compliment'),
+            ('expired', 'Expired')
+        ],
+        default='scheduled'
+    )
+
+    class Meta:
+        db_table = 'state'
