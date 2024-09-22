ebeadf0dfc7343afc1f2c88972e14c72f5260b7d
CarlosHerazo <Carlosherazo2004@gmail.com>
2024-09-22 09:28:22 -0500

    Card de tareas listo

--- static/src/js/index.js
+++ static/src/js/index.js
@@ -1,70 +1,73 @@
 import { ApiRestTask } from "./api.js";
-
 export const RES_READ = async () => {
-  const user_id = document.querySelector("input[name='user_id']").value;
-  let csrftoken = document.cookie.replace(
-      /(?:(?:^|.*;\s*)csrftoken\s*\=\s*([^;]*).*$)|^.*$/,
-      "$1"
-  );
+    const user_id = document.querySelector("input[name='user_id']").value;
+    const csrftoken = document.cookie.match(/csrftoken=([^;]*)/)[1];
 
-  localStorage.csrftoken = csrftoken;
-  localStorage.user_id = user_id;
+    localStorage.csrftoken = csrftoken;
+    localStorage.user_id = user_id;
 
-  const containerCard = document.getElementById("card-container");
-  const apirest = new ApiRestTask(user_id, null, csrftoken);
+    const containerCard = document.getElementById("card-container");
+    const apirest = new ApiRestTask(user_id, null, csrftoken);
 
-  // Esperar la respuesta de read()
-  let res = await apirest.read();
+    try {
+        const res = await apirest.read();
+        // Limpiar el contenedor antes de agregar nuevas tarjetas
+        containerCard.innerHTML = '';
 
-  // Limpiar el contenedor antes de agregar nuevas tarjetas
-  containerCard.innerHTML = '';
+        res.forEach(x => {
+            const [color, text] = getColorAndText(x.state__name);
+            const cardDiv = createCard(x, color, text);
+            containerCard.appendChild(cardDiv);
+        });
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
 
-  // Mapeamos los datos
-  res.map((x) => {
-      let color, text;
+    const card_task = document.querySelectorAll(".card_task")
 
-      switch (x.state__name) {
-          case "scheduled":
-              color = "orange";
-              text = "orange";
-              break;
-          case "to finish":
-              color = "violet";
-              text = "violet";
-              break;
-          case "compliment":
-              color = "green";
-              text = "green";
-              break;
-          case "expired":
-              color = "red";
-              text = "red";
-              break;
-          default:
-              color = "gray"; // Color por defecto
-              text = "gray";
-      }
-
-      let cardDiv = document.createElement('div');
-      cardDiv.id = x.id;
-      cardDiv.className = "flex flex-col m-6 bg-white shadow border border-slate-200 rounded-lg w-96 p-6 hover:shadow-lg cursor-pointer transition-colors duration-300";
-      
-      cardDiv.innerHTML = `
-          <div class="flex items-center mb-4">
-              <i class="fa-solid fa-pencil text-lg"></i>
-              <h5 class="ml-3 text-slate-800 text-2xl font-semibold">${x.name}</h5>
-          </div>
-          <p class="block text-slate-600 leading-normal font-light mb-4">${x.description}</p>
-          <div class="flex justify-between items-center text-sm text-slate-500">
-              <span class="bg-${color}-300 p-1 rounded-lg text-${text}-800">${x.state__name}</span>
-              <span>${x.end_date}</span>
-          </div>
-      `;
-      
-      containerCard.appendChild(cardDiv);
-  });
+    card_task.forEach((btn) => {
+        btn.addEventListener("click", (e) => {
+            console.log(btn.id)
+        })
+    })
+   
 };
+
+function getColorAndText(state) {
+    switch (state) {
+        case "scheduled": return ["orange", "orange"];
+        case "to finish": return ["violet", "violet"];
+        case "compliment": return ["green", "green"];
+        case "expired": return ["red", "red"];
+        default: return ["gray", "gray"];
+    }
+}
+
+function createCard(x, color, text) {
+    const cardDiv = document.createElement('div');
+    cardDiv.id = x.id;
+    cardDiv.className = "flex card_task flex-col m-6 bg-white shadow border border-slate-200 rounded-lg w-96 p-6 hover:shadow-lg cursor-pointer transition-colors duration-300";
+
+    cardDiv.innerHTML = `
+        <div class="flex  items-center mb-4 justify-between">
+            <div class="flex items-center">
+                <i class="fa-solid fa-pencil text-lg"></i>
+                <h5 class="ml-3 text-slate-800 text-2xl font-semibold">${x.name}</h5>
+            </div>
+        </div>
+        <p class="block text-slate-600 leading-normal font-light mb-4">${x.description}</p>
+        <div class="flex justify-between items-center text-sm text-slate-500">
+            <span class="bg-${color}-300 p-1 rounded-lg text-${text}-800">${x.state__name}</span>
+            <span>${x.end_date}</span>
+        </div>
+    `;
+
+    
+    return cardDiv;
+}
+
+
+
 
 // Llama a RES_READ cuando se cargue el DOM
 document.addEventListener('DOMContentLoaded', RES_READ);
-
--- static/src/js/new_task.js
+++ static/src/js/new_task.js
@@ -1,20 +1,45 @@
 import { ApiRestTask,CSRFTOKEN,URL_SERVER_API_READ,USER_ID,notyf } from "./api.js";
 import { RES_READ } from "./index.js";
 import {modalTask  } from "./modal_task.js";
-
 document.querySelector('form').addEventListener('submit', async e => {
-    e.preventDefault();
+    const loading = document.getElementById('loadingIndicator');
+    const modal_task = document.getElementById('modal_task')
+    e.preventDefault(); // Previene el envío predeterminado del formulario
+
+    // Muestra el indicador de carga
+    loading.classList.remove('hidden');
+    modal_task.classList.toggle('opacity-50')
+    // Desactiva la interacción con el formulario
+    e.target.setAttribute('inert', true);
+
+    // Recopila los datos del formulario
     const data = Object.fromEntries(new FormData(e.target));
 
     // Enviar una petición POST
     const apiRest = new ApiRestTask(USER_ID, null, CSRFTOKEN);
     const res = await apiRest.create(data);
-    
+
+    // Manejo de la respuesta
     if (res.message !== "") {
-        
         await RES_READ(); 
         // Mostrar una notificación compacta
         notyf.success(res.message);
-        modalTask.classList.toggle('hidden')
+        modalTask.classList.toggle('hidden'); 
+        // Oculta el indicador de carga
+        loading.classList.add('hidden');
+        document.querySelector('form').reset(); 
+        modal_task.classList.remove('opacity-50')
+            // Reactiva la interacción con el formulario
+        e.target.removeAttribute('inert');
+    }else{
+        notyf.success("ocurrio un error en el proceso");
+        modalTask.classList.toggle('hidden'); 
+        // Oculta el indicador de carga
+        loading.classList.add('hidden');
+        document.querySelector('form').reset(); 
+            // Reactiva la interacción con el formulario
+        e.target.removeAttribute('inert');
+        modal_task.classList.remove('opacity-50')
     }
 });
+ 
